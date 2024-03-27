8d94c86c08c7143ea5406655d3364c8b3a8bf797
yrx <rongxingyang@outlook.com>
2024-03-27 21:31:22 +0800

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,9 @@
+<template>
+    <el-container style="height: 100%; padding:0;">
+      <el-aside width="200px" style="background-color: aliceblue;height: 100%;">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+</template>
